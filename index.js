8409caf6f710d1c7fc0ab243aa6536dd630baccf
Johnson <sakajohnson1@gmail.com>
2024-12-16 00:59:32 +0000

    added a javascript file

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-alert("Welcome , Walker!");
+
+
+let scoreRightElementDisplay = document.getElementById("current-scoreRight");
+
+let scoreLeftElementDisplay = document.getElementById("current-scoreLeft");
+
+
+let currentScoreRight = 0;
+let currentScoreLeft = 0;
+
+// adding score to the left
+
+function add1scoreLeft(){
+  scoreLeftElementDisplay.textContent = currentScoreLeft +=1;
+}
+function add2scoreLeft(){
+  scoreLeftElementDisplay.textContent = currentScoreLeft +=2;
+}
+function add3scoreLeft(){
+  scoreLeftElementDisplay.textContent = currentScoreLeft +=3;
+}
+
+//adding score to the right
+
+function add1scoreRight(){
+  scoreRightElementDisplay.textContent = currentScoreRight +=1;
+}
+function add2scoreRight(){
+  scoreRightElementDisplay.textContent = currentScoreRight +=2;
+}
+
+function add3scoreRight(){
+  scoreRightElementDisplay.textContent = currentScoreRight +=3;
+}
